51675c7618f2e35d6781ce1319b43bc7ce278e7a
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-03-12 14:22:56 +0300

    get students

---
+++ src/components/Students.vue
@@ -0,0 +1,23 @@
+<script>
+import Client from '../../services/api'
+export default {
+  name: 'Students',
+  data: () => ({
+    listOfStudents: []
+  }),
+  methods: {
+    async studentsList() {
+      const response = await Client.get('/students')
+      console.log(response.data)
+      this.listOfStudents = response.data
+    }
+  },
+  mounted() {
+    // studentsList()
+  }
+}
+</script>
+
+<template></template>
+
+<style></style>
