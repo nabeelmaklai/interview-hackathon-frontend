0363ec26d0e23fff253a4e3bcba771cd97421cda
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-03-12 16:13:15 +0300

    student comonent render

--- src/components/Students.vue
+++ src/components/Students.vue
@@ -2,14 +2,20 @@
 import Client from '../../services/api'
 export default {
   name: 'Students',
-  data: () => ({
-    listOfStudents: []
-  }),
+  data: function () {
+    return {
+      listOfStudents: []
+    }
+  },
   methods: {
     async studentsList() {
-      const response = await Client.get('/students')
-      console.log(response.data)
-      this.listOfStudents = response.data
+      try {
+        const response = await Client.get('/students')
+        this.listOfStudents = response.data
+        console.log(response.data)
+      } catch (error) {
+        console.log(error)
+      }
     }
   },
   mounted() {
@@ -18,6 +24,13 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+  <!-- <h3>{{ listOfStudents }}</h3> -->
+  <div v-for="student in listOfStudents">
+    <h3>{{ student.name }}</h3>
+    <h3>{{ student.email }}</h3>
+    <h3>{{ student.studentId }}</h3>
+  </div>
+</template>
 
 <style></style>
--- src/router.js
+++ src/router.js
@@ -3,10 +3,17 @@ import HomePage from './components/HomePage.vue'
 import StudentListing from './components/StudentListing.vue'
 import CourseListing from './components/CourseListing.vue'
 import StudentDetails from './components/StudentDetails.vue'
+import Students from './components/Students.vue'
 
 const routes = [
   { path: '/', component: HomePage, name: 'HomePage' },
-  { path: '/students', component: StudentListing, name: 'StudenTlistings' },
+  {
+    path: '/studentslists',
+    component: StudentListing,
+    name: 'Studentlistings'
+  },
+  { path: '/students', component: Students, name: 'Students' },
+
   { path: '/courses', component: CourseListing, name: 'CourseListings' },
   { path: '/students/:id', component: StudentDetails, name: 'StudentDetails' }
 ]
