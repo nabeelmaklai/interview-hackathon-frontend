b5ad0a23cb2acdddaee17e07c1b0b7cb6fcc16a3
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-03-12 14:59:54 +0300

    commit for a pull

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
-<script setup>
+<script>
 import Students from './components/Students.vue'
+// import Client from './services/api.js'
 
-// export default {
-//   name: 'App',
-//   components: { Students }
-// }
+export default {
+  name: 'App',
+  components: { Students }
+}
 </script>
 
 <template>
