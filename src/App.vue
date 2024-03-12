7b6f26a65f143d9c434109dd264961f8a626d114
AbdulAmir1 <abdulamir0@gmail.com>
2024-03-12 14:56:02 +0300

    added routing function

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
-<script setup></script>
+<script>
+import NavBar from './components/NavBar.vue'
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  },
+  data: () => ({ heading: '' }),
+  methods: {
+    showWallets() {},
+    hideWallets() {}
+  },
+  mounted: function () {}
+}
+</script>
 
 <template>
   <div>
-    <h1>My New Vue App</h1>
+    <h1>The School App</h1>
+    <NavBar />
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/components/StudentDetails.vue
@@ -0,0 +1,20 @@
+<script>
+const students = [{ id: 1, name: 'Ali' }]
+export default {
+  name: 'StudentDetails',
+  data: () => ({ student: null }),
+  mounted: function () {
+    // this.student = students.find(
+    //   (student) => student.id === parseInt(this.$route.params.id)
+    // )
+    console.log('id is : ', parseInt(this.$route.params.id))
+  }
+}
+</script>
+
+<template>
+  <div>Student Details</div>
+  <div>name: {{ student.name }}</div>
+</template>
+
+<style></style>
